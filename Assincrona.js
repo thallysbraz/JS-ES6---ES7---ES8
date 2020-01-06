a965af3759b0c7ff0d4177cc190d943f0c57c1b3
thallysbraz <thallysbraz3@gmail.com>
2020-01-06 14:35:10 -0300

    Promise na pratica

--- Assincrona.js
+++ Assincrona.js
@@ -1,20 +1,24 @@
-function enviarEmail(corpo, para, callback) {
-  setTimeout(() => {
-    var error = true;
-    if (error == true) {
-      callback("falhou, error interno");
-    } else {
-      callback("ok, email enviado");
-    }
-  }, 2000);
+function enviarEmail(corpo, para) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      var error = true;
+      console.log("fora da validação");
+      if (error) {
+        reject();
+      } else {
+        resolve();
+      }
+    }, 2000);
+  });
 }
 console.log("Inicio do envio de email");
 
-enviarEmail("Oi", "thallysbraz3@gmail.com", status => {
-  if (status) {
-    console.log(status);
-  } else {
-    console.log("Seu email foi enviado");
-  }
-});
+enviarEmail("olá", "thallys")
+  .then(() => {
+    console.log("email enviado com sucesso");
+  })
+  .catch(Error => {
+    console.log("deu error");
+  });
+
 console.log("tudo ok");
