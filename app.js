058c8cfa8ca59c903d8fc6ce980c3ca9275c9326
thallysbraz <thallysbraz3@gmail.com>
2020-01-06 10:42:30 -0300

    operador Spread

--- app.js
+++ app.js
@@ -14,14 +14,22 @@ Ex.: const express = require("express");
     let funciona nos escopos Global, Local e Bloco
     var funciona no escopos Global, Local    
 */
-var nome = "Thallys";
+var nomes = "Thallys";
 var sobrenome = "Braz";
 var idade = 21;
 
+var empresa = {
+  nome: "First",
+  cidade: "Brasília",
+  site: "firstdecision.com",
+  email: "gmail@firstdecision.com.br"
+};
+
 var user = {
-  nome,
+  nomes,
   sobrenome,
-  idade
+  idade,
+  ...empresa
 };
 
 console.log(user);
